b3ad0b3cb95591054c93acd3946060f05d6becc1
Ste25 <51375574+Ste25@users.noreply.github.com>
2019-07-05 18:42:12 +0200

    Update Core.php

--- src/Core.php
+++ src/Core.php
@@ -28,36 +28,217 @@ class Core
             'chat' => [0]
         ];
         $this->apiMethod = array();
-        $this->methodName = [
-            'getMe' => 'test',
-            'sendMessage' => 'message',
-            'forwardMessage' => 'forward',
-            'sendPhoto' => 'photo',
-            'sendAudio' => 'audio',
-            'sendDocument' => 'document',
-            'sendVideo' => 'video',
-            'sendAnimation' => 'animation',
-            'sendPoll' => 'poll',
-            'sendChatAction' => 'chatAction',
-            'getUserProfilePhotos' => 'propic',
-            'getFile' => 'file',
-            'kickChatMember' => 'kick',
-            'unbanChatMember' => 'unban',
-            'restrictChatMember' => 'restrict',
-            'promoteChatMember' => 'promote',
-            'exportChatInviteLink' => 'inviteLink',
-            'setChatTitle' => 'chatTitle',
-            'setChatDescription' => 'chatDescription',
-            'pinChatMessage' => 'pin',
-            'unpinChatMessage' => 'unpin',
-            'leaveChat' => 'leave',
-            'getChat' => 'chatInfo',
-            'getChatAdministrators' => 'admins',
-            'getChatMembersCount' => 'membersCount',
-            'getChatMember' => 'member',
-            'setChatStickerSet' => 'setSticker',
-            'deleteChatStickerSet' => 'deleteSticker',
-            'answerCallbackQuery' => 'callbackQuery'
+        $this->methodList = [
+            'getMe' => [
+                'name' => 'test',
+                'params' => [
+                    'chat_id' => false
+                ]
+            ],
+            'sendMessage' => [
+                'name' => 'message',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'forwardMessage' => [
+                'name' => 'forward',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'sendPhoto' => [
+                'name' => 'photo',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'sendAudio' => [
+                'name' => 'audio',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'sendDocument' => [
+                'name' => 'document',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'sendVideo' => [
+                'name' => 'video',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'sendAnimation' => [
+                'name' => 'animation',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'sendPoll' => [
+                'name' => 'poll',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'sendChatAction' => [
+                'name' => 'chatAction',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'getUserProfilePhotos' => [
+                'name' => 'propic',
+                'params' => [
+                    'chat_id' => false
+                ]
+            ],
+            'getFile' => [
+                'name' => 'file',
+                'params' => [
+                    'chat_id' => false
+                ]
+            ],
+            'kickChatMember' => [
+                'name' => 'kick',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'unbanChatMember' => [
+                'name' => 'unban',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'restrictChatMember' => [
+                'name' => 'restrict',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'promoteChatMember' => [
+                'name' => 'promote',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'exportChatInviteLink' => [
+                'name' => 'inviteLink',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'setChatTitle' => [
+                'name' => 'chatTitle',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'setChatDescription' => [
+                'name' => 'chatDescription',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'pinChatMessage' => [
+                'name' => 'pin',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'unpinChatMessage' => [
+                'name' => 'unpin',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'leaveChat' => [
+                'name' => 'leave',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'getChat' => [
+                'name' => 'chatInfo',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'getChatAdministrators' => [
+                'name' => 'admins',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'getChatMembersCount' => [
+                'name' => 'membersCount',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'getChatMember' => [
+                'name' => 'member',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'setChatStickerSet' => [
+                'name' => 'setSticker',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'deleteChatStickerSet' => [
+                'name' => 'deleteSticker',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'answerCallbackQuery' => [
+                'name' => 'callbackQuery',
+                'params' => [
+                    'chat_id' => false
+                ]
+            ],
+            'editMessageText' => [
+                'name' => 'editText',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'editMessageCaption' => [
+                'name' => 'editCaption',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'editMessageMedia' => [
+                'name' => 'editMedia',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'editMessageReplyMarkup' => [
+                'name' => 'editMarkup',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'stopPoll' => [
+                'name' => 'stopPoll',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
+            'deleteMessage' => [
+                'name' => 'deleteMessage',
+                'params' => [
+                    'chat_id' => true
+                ]
+            ],
         ];
         $this->params = array();
         $this->keyboardOptions = [
@@ -80,19 +261,24 @@ class Core
             $ch = curl_init();
             foreach($this->apiMethod as $method) {
                 if(!empty($this->params[$method])) {
-                    foreach($this->chat as $chat) {
-                        if(isset($this->params[$method][0])) {
-                            foreach($this->params[$method][0] as $singleParams) {
-                                $this->execute($ch, $chat, $method, $singleParams);
+                    if($this->methodList[$method]['params']['chat_id']) {
+                        foreach($this->chat as $chat) {
+                            if(isset($this->params[$method][0])) {
+                                foreach($this->params[$method][0] as $singleParams) {
+                                    $this->execute($ch, $chat, $method, $singleParams);
+                                }
+                            }
+                            if(isset($this->params[$method][$chat])) {
+                                foreach($this->params[$method][$chat] as $singleParams) $this->execute($ch, $chat, $method, $singleParams);
                             }
                         }
-                        if(isset($this->params[$method][$chat])) {
-                            foreach($this->params[$method][$chat] as $singleParams) $this->execute($ch, $chat, $method, $singleParams);
-                        }
+                    }
+                    else {
+                        foreach($this->params[$method]['nochat'] as $singleParams) $this->execute($ch, 0, $method, $singleParams);
                     }
                 }
                 else {
-                    if(!empty($this->chat)) {
+                    if(!empty($this->chat) && $this->methodList[$method]['params']['chat_id']) {
                         foreach($this->chat as $chat) {
                             $this->execute($ch, $chat, $method);
                         }
@@ -108,15 +294,20 @@ class Core
 
     /**
      * Perform cURL requests and set responses.
+     * 
+     * @param curl $ch
+     * @param string|int $chat
+     * @param string $method
+     * @param array $params
      */
     private function execute($ch, $chat, $method, $params = null)
     {
-        $this->request['chat'.$chat][$this->methodName[$method]][] = (!empty($chat)) ? ($this->apiUrl . $method . '?chat_id=' . $chat . (empty($params) ? '' : ('&' . http_build_query($params)))) : $this->apiUrl . $method;
-        curl_setopt($ch, CURLOPT_URL, end($this->request['chat'.$chat][$this->methodName[$method]]));
+        $this->request['chat'.$chat][$this->methodList[$method]['name']][] = (!empty($chat)) ? ($this->apiUrl . $method . '?chat_id=' . $chat . (empty($params) ? '' : ('&' . http_build_query($params)))) : $this->apiUrl . $method . (empty($params) ? '' : ('?' . http_build_query($params)));
+        curl_setopt($ch, CURLOPT_URL, end($this->request['chat'.$chat][$this->methodList[$method]['name']]));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $response = json_decode(curl_exec($ch), true);
-        $this->success['chat'.$chat][$this->methodName[$method]][] = $response['ok'];
-        $this->result['chat'.$chat][$this->methodName[$method]][] = isset($response['result']) ? $response['result'] : $response['error_code'] . ' - ' . $response['description'];
+        $this->success['chat'.$chat][$this->methodList[$method]['name']][] = $response['ok'];
+        $this->result['chat'.$chat][$this->methodList[$method]['name']][] = isset($response['result']) ? $response['result'] : $response['error_code'] . ' - ' . $response['description'];
         return;
     }
 
@@ -161,9 +352,17 @@ class Core
 
     /**
      * Add params to list.
+     * 
+     * @param string $method
+     * @param array $params
      */
     protected function addParams($method, $params)
     {
+        if(!$this->methodList[$method]['params']['chat_id']) {
+            if(!isset($this->params[$method]['nochat'])) $this->params[$method]['nochat'] = array();
+            $this->params[$method]['nochat'] = array_merge($this->params[$method]['nochat'], $params);
+            return;
+        }
         foreach($this->currentParams['chat'] as $chat) {
             if(!isset($this->params[$method][$chat])) $this->params[$method][$chat] = array();
             $this->currentParams['index'][$chat] = ['start' => count($this->params[$method][$chat])];
@@ -171,5 +370,35 @@ class Core
             $this->currentParams['index'][$chat]['end'] = count($this->params[$method][$chat]);
         }
         return;
+    }
+
+    /**
+     * Check input and add method to list.
+     * 
+     * @param array $args
+     * @param array $api
+     */
+    protected function setParams($args, $api)
+    {
+        if(!isset($this->apiMethod[$api['name']])) $this->apiMethod[$api['name']] = $api['name'];
+        if(is_array($args[0])) $this->addParams($api['name'], $this->manageParams($args[0], $api['params']));
+        else $this->addParams($api['name'], [$this->changeKeys($args, $api['params'])]);
+        $this->currentParams['method'] = $api['name'];
+        return;
+    }
+
+    /**
+     * Set an array with method's original name and params' name.
+     * 
+     * @param string $name
+     * @param array $params
+     * @return array
+     */
+    protected function setApi($name, $params)
+    {
+        return [
+            'name' => $name,
+            'params' => $params
+        ];
     }
 }
